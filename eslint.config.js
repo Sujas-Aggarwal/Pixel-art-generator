a26b8a23dec319e32318493fd5471e279d440e11
Sujas <ce1221047@iitd.ac.in>
2024-12-22 13:11:26 +0530

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -23,10 +23,10 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-      //undeclared variables and functions 
-      'no-undef': 'warn',
-      //unused variables
-      'no-unused-vars': 'warn',
+      'no-unused-vars': ['warn'], // Warn about unused variables
+      '@typescript-eslint/no-unused-vars': ['warn'], // Warn about unused variables in TypeScript
+      '@typescript-eslint/no-unused-vars-experimental': ['off'], // Disable deprecated rule if included
+      'no-unused-functions': ['warn'], // Warn about unused functions (if custom rule exists)
     },
   },
-)
+);
