91fb91fb1598ffffdb6e0aa4be4d35ce00f05df6
Sujas <ce1221047@iitd.ac.in>
2024-12-23 12:21:42 +0530

    added keyboard shortcut

--- src/components/RightToolbar.tsx
+++ src/components/RightToolbar.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useToolStore } from "../context/ToolStore";
 import { Tool } from "../types/Tools";
-
+import "../css/RightToolbar.css";
 function RightToolbar() {
   const {
     color,
@@ -14,53 +14,66 @@ function RightToolbar() {
     redo,
     rotateCW,
     rotateCCW,
-    bucketFill,
   } = useToolStore();
   useEffect(() => {
-    document.addEventListener("keydown", (event) => {
-      if (event.ctrlKey && event.key === "z") {
-        undo();
-      }
-      if (event.ctrlKey && event.key === "y") {
-        redo();
-      }
-    });
+    document.addEventListener("keydown", keyboardShortcuts);
     return () => {
-      document.removeEventListener("keydown", (event) => {
-        if (event.ctrlKey && event.key === "z") {
-          undo();
-        }
-        if (event.ctrlKey && event.key === "y") {
-          redo();
-        }
-      });
+      document.removeEventListener("keydown", keyboardShortcuts);
     };
   }, []);
+
+  const keyboardShortcuts = (event: KeyboardEvent) => {
+    if (event.ctrlKey && event.key === "r") {
+      return;
+    }
+    event.preventDefault();
+    if (event.ctrlKey && event.key === "z") {
+      undo();
+    }
+    if (event.ctrlKey && event.key === "y") {
+      redo();
+    }
+    if (event.key === "r") {
+      rotateCW();
+    }
+    if (event.key === "l") {
+      rotateCCW();
+    }
+    if (event.key === "b") {
+      setSelectedTool(Tool.BucketFill);
+    }
+    if (event.key === "p") {
+      setSelectedTool(Tool.Pencil);
+    }
+    if (event.key === "e") {
+      setSelectedTool(Tool.Eraser);
+    }
+  };
   return (
-    <div className="toolbar bg-[#d9bda5] w-[200px]  flex justify-center items-center">
-      <h1 className="text-center">Toolbar Space</h1>
-      <div className="flex flex-col items-center">
+    <div className="bg-[#d9bda5] w-[200px]  flex justify-center items-center p-2">
+      <div
+        id="toolbar-right"
+        className="flex flex-col justify-center items-center bg-[#e8cbb1] p-2 w-full"
+      >
         <input
           type="color"
           value={color}
           onChange={(e) => setColor(e.target.value)}
-          className="w-[100px] h-[50px] rounded-lg"
+          className="p-0"
         />
         <input
           type="number"
           value={pixelCount}
+          style={{
+            WebkitAppearance: "none",
+            MozAppearance: "textfield",
+          }}
           onChange={(e) => setPixelCount(parseInt(e.target.value))}
-          className="w-[100px] h-[50px] rounded-lg"
+          className="p-0"
         />
-        <select
-          value={selectedTool}
-          onChange={(e) => setSelectedTool(e.target.value as any)}
-          className="w-[100px] h-[50px] rounded-lg"
-        >
-          <option value={Tool.Pencil}>Pencil</option>
-          <option value={Tool.Eraser}>Eraser</option>
-          <option value={Tool.BucketFill}>Bucket Fill</option>
-        </select>
+        <button onClick={()=>{setSelectedTool(Tool.Pencil)}}>Pencil</button>
+        <button onClick={()=>{setSelectedTool(Tool.Eraser)}}>Eraser</button>
+        <button onClick={()=>{setSelectedTool(Tool.BucketFill)}}>BucketFill</button>
         <button onClick={undo}>Undo</button>
         <button onClick={redo}>Redo</button>
         <button onClick={rotateCW}>Rotate CW</button>
