ed21cc2e7809ab780be1b7aa578e7c7e27abe611
Prashant Rewar <prashantrewarofficial@gmail.com>
2025-02-06 02:38:01 +0530

    Add files via upload

--- src/components/LeftToolbar.tsx
+++ src/components/LeftToolbar.tsx
@@ -1,13 +1,16 @@
 import { useToolStore } from "../context/ToolStore";
 
 function LeftToolbar() {
-    const {exportImage} = useToolStore();
+    const {exportImage , exportVector } = useToolStore();
   return (
 
     <div className="w-[100px] min-w-[100px] bg-[#d9bda5] flex flex-col justify-between py-4 items-center">
       <img src="/favicon2.png" alt="" className="p-2 max-w-[80px]"/>
       <h1 className="text-center">Online Free Pixel Art Generator</h1>
-      <button  onClick={exportImage} className="bg-[#804816] text-white p-2 text-sm  font-bold uppercase">Save Art</button>
+      <div className="flex flex-col gap-2">
+      <button  onClick={exportVector} className="bg-[#804816] text-white p-2 text-sm  font-bold uppercase">Save SVG</button>
+      <button  onClick={exportImage} className="bg-[#804816] text-white p-2 text-sm  font-bold uppercase">Save PNG</button>
+      </div>
     </div>
   );
 }
